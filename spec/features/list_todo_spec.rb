7ccd40192359d05fc82ff80631ac1aaf7fe0029d
Guilherme A. Dallanol <guilherme.dallanol@gmail.com>
2018-04-11 00:13:26 -0300

    Lists Todos dynamically

--- spec/features/list_todo_spec.rb
+++ spec/features/list_todo_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 feature 'Listing Todos' do
+  background do
+    @todo = TodoItem.create(description: 'Comprar Leite')
+  end
+
   scenario 'lists all Todos' do
     visit todos_path
 
     expect(page).to have_text("TODOS")
+    expect(page).to have_text("Comprar Leite")
   end
 end
