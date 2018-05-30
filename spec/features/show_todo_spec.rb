5b984b346a3639e3eb093d7cf81ba951757da2a1
Victor Alexandre <victor.alexandrefs@gmail.com>
2018-05-29 23:39:21 -0300

    Handles locale on todo_items index and show

--- spec/features/show_todo_spec.rb
+++ spec/features/show_todo_spec.rb
@@ -5,9 +5,17 @@ feature 'Showing todo' do
     @todo = TodoItem.create(description: 'Comprar Leite')
   end
 
-  scenario 'Display details' do
-    visit todo_item_path(@todo)
-
+  scenario 'Display details in English' do
+    visit todo_item_path(@todo, locale: :en)
+    expect(page).to have_text("Details to-do")
     expect(page).to have_text("Comprar Leite")
+    expect(page).to have_link('Edit', href: edit_todo_item_path(@todo, locale: :en))
+  end
+
+  scenario 'Display details in Brazilian Portuguese' do
+    visit todo_item_path(@todo, locale: 'pt-BR')
+    expect(page).to have_text("Detalhes da tarefa")
+    expect(page).to have_text("Comprar Leite")
+    expect(page).to have_link('Editar', href: edit_todo_item_path(@todo, locale: 'pt-BR'))
   end
 end
