2ce0600782503a8aa9eccdf3da9dea703f844a75
Raul Fernando <raulfernando08@gmail.com>
2018-07-03 22:31:04 -0300

    Implements user model

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'bcrypt', '~> 3.1', '>= 3.1.12'
 
 group :development, :test do
   gem 'capybara', '~> 2.13'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411025440) do
+ActiveRecord::Schema.define(version: 20180704011816) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
