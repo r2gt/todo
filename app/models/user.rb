bc92609ce3d8defaf1bcdd2e96ce057f460ccf11
Raul Fernando <raulfernando08@gmail.com>
2018-07-03 23:10:12 -0300

    Implements validation user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :name, :username, :email, :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 describe User do
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :username }
+  it { is_expected.to validate_presence_of :email }
+  it { is_expected.to validate_presence_of :password }
 end
