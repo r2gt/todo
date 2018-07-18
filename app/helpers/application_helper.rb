7d639097cb5200e41736d4ec757af54b43245ca4
Victor Alexandre <victor.alexandrefs@gmail.com>
2018-07-17 22:38:31 -0300

    Fixes locale link to avoid wrong locale mapping

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   LOCALE_LABELS = { 'en' => 'English', 'pt-BR' => 'Português' }.freeze
 
   def locale_links(exclude:)
-    (locales = LOCALE_LABELS.dup).delete(exclude)
+    (locales = LOCALE_LABELS.dup).delete(exclude.to_s)
 
     locales.map do |locale_type, locale_label|
       link_to locale_label, locale_root_path(locale: locale_type)
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe ApplicationHelper do
   describe '#locale_links exclude:' do
-    it 'returns a map of links for accepted locales, without excluded locale option' do
+    it 'returns a collection of links for accepted locales, without excluded locale option' do
       expect(
         helper.locale_links(exclude: 'pt-BR')
       ).to eql ["<a href=\"/en\">English</a>"]
