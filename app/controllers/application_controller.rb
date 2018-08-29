0037106d3eb8fa53522c85a28e8c4fa3409d6d70
TheanRogerio <thean.rogerio@gmail.com>
2018-08-28 23:29:15 -0300

    Define current_user and logged_in? methods in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  helper_method :current_user, :logged_in?
   before_action :set_locale
 
   def default_url_options
     { locale: I18n.locale }
   end
 
+  def current_user
+    @current_user ||= User.find(session[:current_user_id]) if session[:current_user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+  
   private
 
   def extract_locale
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,4 +4,47 @@ describe ApplicationController do
   it '#default_url_options' do
     expect(subject.default_url_options).to eql(locale: I18n.locale)
   end
+
+  describe '#current_user' do
+    let(:user) do
+      User.create(
+        name: 'Foo', username: 'Bar', email: 'email@email.com', 
+        password: 'asdf12345'
+      )
+    end
+
+    context 'With current_user_id on session' do
+      it 'returns user found by id' do
+        allow(
+          subject
+        ).to receive(:session).and_return(current_user_id: user.id)
+
+        expect(subject.current_user).to eql(user)
+      end
+    end
+
+    context 'without current_user_id on session' do
+      it 'returns nil' do
+        expect(subject.current_user).to be_nil
+      end
+    end
+  end
+
+  describe '#logged_in?' do
+    let(:user) { double(:user) }
+
+    context 'when current_user exists' do
+      it 'is logged in' do
+        expect(subject).to receive(:current_user).once.and_return(user)
+        expect(subject).to be_logged_in
+      end
+    end
+
+    context 'when current user is nil' do
+      it "isn't logged in" do
+        expect(subject).to receive(:current_user).once.and_return(nil)
+        expect(subject).not_to be_logged_in
+      end
+    end
+  end
 end
