0dd13c0e15fc81037bdb763324cef73d8b4efcfd
TheanRogerio <thean.rogerio@gmail.com>
2019-01-08 23:02:49 -0200

    Add api_token to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181128013129) do
+ActiveRecord::Schema.define(version: 20190108231012) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string "description"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20181128013129) do
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_token"
   end
 
 end
